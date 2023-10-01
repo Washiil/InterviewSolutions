779c5cc597717decfc6c0bacd0f8b48618f59133
Washiil <65116802+Washiil@users.noreply.github.com>
2023-09-30 23:05:56 -0400

    leetcode 557

---
+++ LeetCode/557. Reverse Words in a String III.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split(' ')
+        rev_words = []
+        for x in words:
+            rev_words.append(x[::-1])
+
+        return ' '.join(rev_words)
+        
