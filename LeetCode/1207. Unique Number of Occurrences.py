dbb50bbe93d33418e10e1f90cc849dd3ff22aef6
Washiil <65116802+Washiil@users.noreply.github.com>
2023-10-15 20:02:43 -0400

    Leetcode 1207

---
+++ LeetCode/1207. Unique Number of Occurrences.py
@@ -0,0 +1,8 @@
+def uniqueOccurrences(self, arr: list[int]) -> bool:
+		count = {}
+		for val in arr:
+				count[val] = count.get(val, 0) + 1
+
+		if len(set(count.values())) == len(count.values()):
+				return True
+		return False
