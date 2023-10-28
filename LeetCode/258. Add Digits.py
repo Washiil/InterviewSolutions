04224c012e3fdec481fed3d36c2d1d94b8f011cf
Washiil <65116802+Washiil@users.noreply.github.com>
2023-10-28 19:24:35 -0400

    Leetcode 258

---
+++ LeetCode/258. Add Digits.py
@@ -0,0 +1,12 @@
+def addDigits(self, num: int) -> int:
+		while True:
+				total = 0
+				while num > 0:
+						d = num % 10 # Get the last digit of the number
+						total += d   # Add to sum
+						num //= 10   # Remove the digit
+				
+				if total < 10:   # Simple single digit check
+						return total
+				else:
+						num = total  # Reset for next loop
