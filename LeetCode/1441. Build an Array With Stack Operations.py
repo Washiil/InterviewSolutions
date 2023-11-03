63088ed795d1654997c5f60573c86661d28685d4
Washiil <65116802+Washiil@users.noreply.github.com>
2023-11-02 21:42:34 -0400

    Format 1441?

--- LeetCode/1441. Build an Array With Stack Operations.py
+++ LeetCode/1441. Build an Array With Stack Operations.py
@@ -1,11 +1,11 @@
 def buildArray(self, target: list[int], n: int) -> list[str]:
-		result = []
+    result = []
 
-		for i in range(1, n + 1):
-				if i in target:
-						result.append("Push")
-				else:
-						result.extend(["Push", "Pop"])
-				if i == target[-1]:
-						break
-		return result
+    for i in range(1, n + 1):
+        if i in target:
+            result.append("Push")
+        else:
+            result.extend(["Push", "Pop"])
+        if i == target[-1]:
+            break
+    return result
