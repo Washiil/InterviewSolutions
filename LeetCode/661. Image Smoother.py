9253349618dc4dd52f40079d8092cca1215fa354
Washiil <65116802+Washiil@users.noreply.github.com>
2023-12-18 20:38:35 -0500

    Leetcode 661

---
+++ LeetCode/661. Image Smoother.py
@@ -0,0 +1,21 @@
+def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+    rows = len(img)
+    cols = len(img[0])
+
+    output = [[0] * cols for i in range(rows)]
+
+    for i in range(rows):
+        for j in range(cols):
+            pixel_total = 0
+            pixel_count = 0
+
+            # Nice and neat solution to handling upper and lower index out of bounds
+            # Credit: https://leetcode.com/MrAke/
+            for x in range(max(0, i - 1), min(rows, i + 2)):
+                for y in range(max(0, j - 1), min(cols, j + 2)):
+                    pixel_total += img[x][y]
+                    pixel_count += 1
+
+            output[i][j] = pixel_total // pixel_count
+
+    return output
