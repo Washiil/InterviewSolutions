d606ca0308daaaf22af06937d11188706dc37a14
Washiil <65116802+Washiil@users.noreply.github.com>
2024-01-22 23:20:58 -0500

    Leetcode 645 Python

---
+++ python/LeetCode/645. Set Mismatch.py
@@ -0,0 +1,11 @@
+def findErrorNums(self, nums: list[int]) -> list[int]:
+    dup, missing = -1, -1
+    
+    for i in range(1, len(nums) + 1):
+        count = nums.count(i)
+        if count == 2:
+            dup = i
+        elif count == 0:
+            missing = i
+    
+    return [dup, missing]
