b66a7623aa81d0f90969a8d92f77edfc21bddcf6
Washi <65116802+Washiil@users.noreply.github.com>
2024-02-21 21:30:38 -0500

    Leetcode 997 Python

---
+++ python/LeetCode/997. Find the Town Judge.py
@@ -0,0 +1,12 @@
+def findJudge(self, n: int, trust: List[List[int]]) -> int:
+    people = [0] * (n + 1)
+
+    for a,b in trust:
+        people[a] -= 1
+        people[b] += 1
+    
+    for i in range(1, n + 1):
+        if people[i] == n -1:
+            return i
+    
+    return -1
