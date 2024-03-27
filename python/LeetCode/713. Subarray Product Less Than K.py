5baeb8ae39ab8d2de8e092ce9d4c4dadb1c6e5e3
Washi <65116802+Washiil@users.noreply.github.com>
2024-03-26 21:08:22 -0400

    Update 713. Subarray Product Less Than K.py

--- python/LeetCode/713. Subarray Product Less Than K.py
+++ python/LeetCode/713. Subarray Product Less Than K.py
@@ -1,18 +1,20 @@
-    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        if k <= 1:
-            return 0
+from typing import List
 
-        total_count = 0
-        product = 1
+def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+    if k <= 1:
+        return 0
 
-        left = 0
-        for right, num in enumerate(nums):
-            product *= num
+    total_count = 0
+    product = 1
 
-            while product >= k:
-                product //= nums[left]
-                left += 1
+    left = 0
+    for right, num in enumerate(nums):
+        product *= num
 
-            total_count += right - left + 1
+        while product >= k:
+            product //= nums[left]
+            left += 1
 
-        return total_count
+        total_count += right - left + 1
+
+    return total_count
