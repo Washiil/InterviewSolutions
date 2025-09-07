2f99cb56b06f8355e56bbcbed62addf87d0606c0
Washi <65116802+Washiil@users.noreply.github.com>
2025-09-06 22:46:25 -0400

    Implement sumZero function to generate zero-sum array

---
+++ python/LeetCode/01304.py
@@ -0,0 +1,13 @@
+def sumZero(self, n: int) -> List[int]:
+    arr = []
+    if n % 2 == 0:
+        for i in range(1, (n // 2) + 1):
+            arr.append(i)
+            arr.append(i * -1)
+    else:
+        arr.append(0)
+        for i in range(1, (n // 2) + 1):
+            arr.append(i)
+            arr.append(i * -1)
+    
+    return arr
