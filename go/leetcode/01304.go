057d774a6163fc4200eb2f5cb77149c180edd2ae
Washi <65116802+Washiil@users.noreply.github.com>
2025-09-06 22:58:24 -0400

    Leetcode golang 01304

---
+++ go/leetcode/01304.go
@@ -0,0 +1,15 @@
+func sumZero(n int) []int {
+    arr := make([]int, 0, 100)
+    limit := (n / 2) + 1
+
+    if n % 2 != 0 {
+        arr = append(arr, 0)
+    }
+
+    for i := 1; i < limit; i++ {
+        arr = append(arr, i)
+        arr = append(arr, i * -1)
+    }
+
+    return arr
+}
