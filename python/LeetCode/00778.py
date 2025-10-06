844d229f3fe7f2bd27046d819e5cc5e94b43b267
Washi <65116802+Washiil@users.noreply.github.com>
2025-10-05 22:55:33 -0400

    binary search

--- python/LeetCode/00778.py
+++ python/LeetCode/00778.py
@@ -13,7 +13,6 @@ class Solution:
 
         visited[r][c] = 1
 
-        # Explore all four directions
         if self.dfs(r + 1, c, grid, visited, t):
             return True
         if self.dfs(r - 1, c, grid, visited, t):
@@ -23,38 +22,26 @@ class Solution:
         if self.dfs(r, c - 1, grid, visited, t):
             return True
 
-        # Backtracking: un-mark the cell if no path was found from this point
         visited[r][c] = 0
 
         return False
 
 
     def swimInWater(self, grid: List[List[int]]) -> int:
-        t = 0
         n = len(grid)
-
-        lower, upper = 0, 0, 0
-
-        for i in range(n):
-            for j in range(n):
-                lower = min(lower, grid[i][j])
-                upper = max(upper, grid[i][j])
-
-        mins = []
-        for i in range(1, n-1):
-            mins.append(min(grid[i]))
-            mins.append(min([grid[i][j] for j in range(n)]))
         
-        if len(mins) > 0:
-            t += max(mins)
-        
-        while lower < upper:
+        lower = grid[0][0]
+        upper = n * n - 1
+        ans = upper
+
+        while lower <= upper:
             mid = (lower + upper) // 2
             visited = [[0 for _ in range(n)] for _ in range(n)]
-            if self.dfs(0, 0, grid, visited, t):
-                t = mid
-                upper = mid-1
-            else:
-                lower = mid+1
 
-        return t
+            if self.dfs(0, 0, grid, visited, mid):
+                ans = mid
+                upper = mid - 1
+            else:
+                lower = mid + 1
+
+        return ans
