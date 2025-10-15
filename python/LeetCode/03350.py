1f614399d2cf9ceda9a1d263a9c9d51374714180
Washi <65116802+Washiil@users.noreply.github.com>
2025-10-14 23:01:40 -0400

    final dp appraoch

--- python/LeetCode/03350.py
+++ python/LeetCode/03350.py
@@ -1,25 +1,28 @@
+from typing import List
+
 class Solution:
-    def maxIncreasingSubarraysV2(self, nums: List[int]) -> int:
+    def maxIncreasingSubarrays(self, nums: List[int]) -> int:
         n = len(nums)
         if n < 2:
             return 0
-
-        inc_from_left = [1] * n
+        
+        run_lengths = []
+        current_length = 1
         for i in range(1, n):
             if nums[i] > nums[i-1]:
-                inc_from_left[i] = inc_from_left[i-1] + 1
-        
-        inc_from_right = [1] * n
-        for i in range(n - 2, -1, -1):
-            if nums[i] < nums[i+1]:
-                inc_from_right[i] = inc_from_right[i+1] + 1
-        
+                current_length += 1
+            else:
+                run_lengths.append(current_length)
+                current_length = 1
+        run_lengths.append(current_length) # Add the last run
+
         max_k = 0
-        for i in range(n - 1):
-            k_from_adjacent = min(inc_from_left[i], inc_from_right[i+1])
+        
+        for length in run_lengths:
+            max_k = max(max_k, length // 2)
+            
+        for i in range(len(run_lengths) - 1):
+            k_from_adjacent = min(run_lengths[i], run_lengths[i+1])
             max_k = max(max_k, k_from_adjacent)
-
-        for length in inc_from_left:
-             max_k = max(max_k, length // 2)
-
+            
         return max_k
