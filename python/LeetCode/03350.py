7f2b0251b4cc51afac463c6056b1c2bab21d2a44
Washi <65116802+Washiil@users.noreply.github.com>
2025-10-14 23:00:50 -0400

    dynamic programming appraoch

--- python/LeetCode/03350.py
+++ python/LeetCode/03350.py
@@ -1,22 +1,25 @@
 class Solution:
-    def maxIncreasingSubarraysV1(self, nums: List[int]) -> int:
-        if len(nums) < 2:
+    def maxIncreasingSubarraysV2(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
             return 0
-            
-        max_k = 0
-        previous_run_length = 0
-        current_run_length = 1
 
-        for i in range(1, len(nums)):
+        inc_from_left = [1] * n
+        for i in range(1, n):
             if nums[i] > nums[i-1]:
-                current_run_length += 1
-            else:
-                # A run just ended. Update max_k based on this and the previous run.
-                max_k = max(max_k, current_run_length // 2, min(current_run_length, previous_run_length))
-                previous_run_length = current_run_length
-                current_run_length = 1
-
-        # Final check for the last run in the array
-        max_k = max(max_k, current_run_length // 2, min(current_run_length, previous_run_length))
+                inc_from_left[i] = inc_from_left[i-1] + 1
         
+        inc_from_right = [1] * n
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i+1]:
+                inc_from_right[i] = inc_from_right[i+1] + 1
+        
+        max_k = 0
+        for i in range(n - 1):
+            k_from_adjacent = min(inc_from_left[i], inc_from_right[i+1])
+            max_k = max(max_k, k_from_adjacent)
+
+        for length in inc_from_left:
+             max_k = max(max_k, length // 2)
+
         return max_k
