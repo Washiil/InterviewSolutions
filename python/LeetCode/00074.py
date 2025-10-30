18b6c53b065189bd41e67ba59d368263c192f535
Washi <65116802+Washiil@users.noreply.github.com>
2025-10-29 23:36:56 -0400

    Leetcode 74 python

---
+++ python/LeetCode/00074.py
@@ -0,0 +1,25 @@
+from typing import List
+
+def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    high = (rows * cols) - 1
+    low = 0
+    mid = 0
+
+    while low <= high:
+        mid = (low + high) // 2
+        row = mid // cols
+        col = mid % cols
+
+        guess = matrix[row][col]
+
+        if guess > target:
+            high = mid - 1
+        elif guess < target:
+            low = mid + 1
+        elif guess == target:
+            return True
+
+    return False
