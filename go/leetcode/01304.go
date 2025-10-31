92e3cd2ddc4c3f207d55fdaf928608d0cd380382
Washi <65116802+Washiil@users.noreply.github.com>
2025-10-30 23:10:42 -0400

    fixed undefined package

--- go/leetcode/01304.go
+++ go/leetcode/01304.go
@@ -1,15 +1,17 @@
+package main
+
 func sumZero(n int) []int {
-    arr := make([]int, 0, 100)
-    limit := (n / 2) + 1
+	arr := make([]int, 0, 100)
+	limit := (n / 2) + 1
 
-    if n % 2 != 0 {
-        arr = append(arr, 0)
-    }
+	if n%2 != 0 {
+		arr = append(arr, 0)
+	}
 
-    for i := 1; i < limit; i++ {
-        arr = append(arr, i)
-        arr = append(arr, i * -1)
-    }
+	for i := 1; i < limit; i++ {
+		arr = append(arr, i)
+		arr = append(arr, i*-1)
+	}
 
-    return arr
+	return arr
 }
