2ccb4f18792fddf2d4a963a70ea49d71e3b6cac2
Washi <65116802+Washiil@users.noreply.github.com>
2025-11-17 23:31:14 -0500

    Leetcode 00717 golang

---
+++ go/leetcode/00717.go
@@ -0,0 +1,14 @@
+func isOneBitCharacter(bits []int) bool {
+    n := len(bits)
+    
+    i := 0
+    for i < n - 1 {
+        if bits[i] == 1 {
+            i += 2
+        } else {
+            i += 1
+        }
+    }
+
+    return i == n - 1;
+}
