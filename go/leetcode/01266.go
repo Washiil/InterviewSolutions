094104fe8a5f1dac97ba1abf00381eb584a6b51b
Washi <65116802+Washiil@users.noreply.github.com>
2026-01-11 22:56:11 -0500

    leetcode 1266 golang

---
+++ go/leetcode/01266.go
@@ -0,0 +1,18 @@
+func intAbs(val int) int {
+	if val < 0 {
+		return val * -1
+	}
+	return val
+}
+
+func minTimeToVisitAllPoints(points [][]int) int {
+	distance := 0
+
+	for i := 1; i < len(points); i++ {
+		x1, y1 := points[i-1][0], points[i-1][1]
+		x2, y2 := points[i][0], points[i][1]
+
+		distance += max(intAbs(x2-x1), intAbs(y2-y1))
+	}
+	return distance
+}
