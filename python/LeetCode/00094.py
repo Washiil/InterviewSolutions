1c9d63d391fe3d3907f97dec344e3e78f78f05e7
Washiil <65116802+Washiil@users.noreply.github.com>
2026-01-12 22:22:16 -0500

    Leetcode 94 Python

---
+++ python/LeetCode/00094.py
@@ -0,0 +1,13 @@
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    if root is None:
+        return []
+    
+    return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
