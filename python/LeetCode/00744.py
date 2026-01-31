46939957b5fe7311266a3f8c04411542c598b098
Washi <65116802+Washiil@users.noreply.github.com>
2026-01-30 22:28:23 -0500

    Leetcode 744 Python with binary search

--- python/LeetCode/00744.py
+++ python/LeetCode/00744.py
@@ -1,7 +1,18 @@
 from typing import List
 
 def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-    for c in letters:
-        if c > target:
-            return c
-    return letters[0]
+    if target >= letters[-1]:
+        return letters[0]
+
+    n = len(letters)
+
+    low = 0
+    high = n - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if letters[mid] <= target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return letters[low]
