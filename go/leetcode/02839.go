059504c4b47aa841dc20b53dd9d19448f0f8ba3c
Washi <65116802+Washiil@users.noreply.github.com>
2026-03-28 23:09:51 -0400

    Leetcode 2839 golang

---
+++ go/leetcode/02839.go
@@ -0,0 +1,9 @@
+func canBeEqual(s1 string, s2 string) bool {
+	// Even indices
+	evenMatch := (s1[0] == s2[0] && s1[2] == s2[2]) || (s1[0] == s2[2] && s1[2] == s2[0])
+
+	// Odd indices
+	oddMatch := (s1[1] == s2[1] && s1[3] == s2[3]) || (s1[1] == s2[3] && s1[3] == s2[1])
+
+	return evenMatch && oddMatch
+}
