de9f81c4b93dc11def565145722c484163ff29d0
Washi <65116802+Washiil@users.noreply.github.com>
2026-06-01 23:07:47 -0400

    Leetcode 3633 Python

---
+++ python/LeetCode/03633.py
@@ -0,0 +1,20 @@
+def earliestFinishTime(
+    self,
+    landStartTime: list[int],
+    landDuration: list[int],
+    waterStartTime: list[int],
+    waterDuration: list[int],
+) -> int:
+    n = len(landStartTime)
+    m = len(waterStartTime)
+    res = float('inf')
+    for i in range(n):
+        for j in range(m):
+            land = landStartTime[i] + landDuration[i]
+            land_water = max(land, waterStartTime[j]) + waterDuration[j]
+            res = min(res, land_water)
+
+            water = waterStartTime[j] + waterDuration[j]
+            water_land = max(water, landStartTime[i]) + landDuration[i]
+            res = min(res, water_land)
+    return res
